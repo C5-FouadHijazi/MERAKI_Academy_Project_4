00a7e6ab88cd9fab3304bb6f4fd1880bf463f722
FouadHijazi <fouad.o.hijazi@gmail.com>
2022-05-15 11:34:53 +0300

    add login

--- starter-code/backend/routes/login.js
+++ starter-code/backend/routes/login.js
@@ -9,11 +9,16 @@ const loginRouter = express.Router();
 /*
  * Testing Routes:
  * POST -> http://localhost:5000/login/
-*/
+ */
 
 /*
  * Testing Object:
 {
+  "Admin"
+   "email":"fouadhijazi92@gmail.com",
+  "password": "123456",
+
+  "User"
   "email":"basher@gmail.com",
   "password": "123456"
 }
