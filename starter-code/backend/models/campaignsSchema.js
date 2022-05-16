33a95d93cf66f68f33cab12d5015ede04f7abe2a
FouadHijazi <fouad.o.hijazi@gmail.com>
2022-05-16 14:25:08 +0300

    Edit Situation Functions

--- starter-code/backend/models/campaignsSchema.js
+++ starter-code/backend/models/campaignsSchema.js
@@ -5,7 +5,7 @@ const campaigns = new mongoose.Schema({
   description: { type: String, required: true },
   img: { type: String, required: true },
   // catId from situation
-  situation: { type: mongoose.Schema.Types.ObjectId, ref: "Situations" },
+  situation: [{ type: mongoose.Schema.Types.ObjectId, ref: "Situations" }],
 });
 
 module.exports = mongoose.model("Campaigns", campaigns);
