09f8f2bc110953680fe5c532376231b55af45b89
FouadHijazi <fouad.o.hijazi@gmail.com>
2022-05-16 14:12:28 +0300

    Craete Header with Nav compo

--- starter-code/frontend/src/components/Navbar/Navbar.js
+++ starter-code/frontend/src/components/Navbar/Navbar.js
@@ -3,7 +3,6 @@ import React, { useState, useContext } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { tokenContext } from "../../App";
 
-
 const Navbar = () => {
   const [clciked, setClciked] = useState(false);
 
@@ -17,9 +16,9 @@ const Navbar = () => {
     setToken("");
   };
 
-  const handleClick = () => {
+ /*  const handleClick = () => {
     setClciked(true);
-  };
+  }; */
 
   return (
     <div className="Navbar">
@@ -30,40 +29,54 @@ const Navbar = () => {
       <ul className="Nav-menu">
         {islogin === true ? (
           <>
-            <li className="nav-link">
-              <Link to="/Home">Home</Link>
+            <li >
+              <Link className="nav-link" to="/Home">Home</Link>
             </li>
 
-            <li className="nav-link">
-              <Link to="/Campaigns">Campaigns</Link>
+            <li>
+              <Link className="nav-link" to="/Campaigns">
+                Campaigns
+              </Link>
             </li>
 
-            <li className="nav-link">
-              <Link to="/ContactUs">Us</Link>
+            <li>
+              <Link className="nav-link" to="/ContactUs">
+                Us
+              </Link>
             </li>
 
-            <li className="nav-link">
-              <Link to="/logout" onClick={logOut}>
+            <li>
+              <Link className="nav-link" to="/logout" onClick={logOut}>
                 logout
               </Link>
             </li>
           </>
         ) : (
           <>
-            <li className="nav-link">
-              <Link to="/Home">Home</Link>
+            <li>
+              <Link className="nav-link" to="/Home">
+                Home
+              </Link>
             </li>
-            <li className="nav-link">
-              <Link to="/Campaigns">Campaigns</Link>
+            <li>
+              <Link className="nav-link" to="/Campaigns">
+                Campaigns
+              </Link>
             </li>
-            <li className="nav-link">
-              <Link to="/Register/">Register</Link>
+            <li>
+              <Link className="nav-link" to="/Register/">
+                Register
+              </Link>
             </li>
-            <li className="nav-link">
-              <Link to="/login">login</Link>
+            <li>
+              <Link className="nav-link" to="/login">
+                login
+              </Link>
             </li>
-            <li className="nav-link">
-              <Link to="/ContactUs">Us</Link>
+            <li>
+              <Link className="nav-link" to="/ContactUs">
+                Us
+              </Link>
             </li>
           </>
         )}
